6ccca09e93417e9c100a45079bfea3848ea77a66
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2013-01-06 00:52:15 +0000

    Basic page scraper working

--- recommenderSystem/pageScraper.py
+++ recommenderSystem/pageScraper.py
@@ -1,5 +1,16 @@
 import urllib2
 
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
-soup = 
+url = "http://uk.gamespot.com/games.html?platform=1029&mode=all&sort=views&dlx_type=all&sortdir=asc&official=all"
+soup = BeautifulSoup(urllib2.urlopen(url))
+
+gamesTable = soup.find("tbody").find_all("tr")
+
+for game in gamesTable:
+	title = game.find("a").string
+	details = game.find_all("td")
+	genre = details[0].string
+	score = details[1].string
+	releaseDate = details[2].string
+	print(title + " " + genre + " " + score + " " + releaseDate)
