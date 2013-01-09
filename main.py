c6bd64b8bc40c261caf602f74a2e77c63de79c5d
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2013-01-09 15:37:21 +0000

    Page Scraper - printing more info

--- main.py
+++ main.py
@@ -15,11 +15,13 @@
 # limitations under the License.
 #
 import webapp2
+import os
+
+from google.appengine.ext.webapp import template
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+    	path = os.path.join(os.path.dirname(__file__), 'index.html')
+        self.response.out.write(template.render(path, {}))
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+app = webapp2.WSGIApplication([('/.*', MainHandler)], debug=True)
--- pageScraper.py
+++ pageScraper.py
@@ -2,10 +2,12 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
-url = "http://uk.gamespot.com/games.html?platform=1029&mode=all&sort=views&dlx_type=all&sortdir=asc&official=all"
-soup = BeautifulSoup(urllib2.urlopen(url))
+#shouldn't the code be neater?!
 
-gamesTable = soup.find("tbody").find_all("tr")
+url = "http://uk.gamespot.com/games.html?platform=1029&mode=all&sort=views&dlx_type=all&sortdir=asc&official=all"
+pageSoup = BeautifulSoup(urllib2.urlopen(url))
+
+gamesTable = pageSoup.find("tbody").find_all("tr")
 
 for game in gamesTable:
 	title = game.find("a").string
@@ -13,4 +15,5 @@ for game in gamesTable:
 	genre = details[0].string
 	score = details[1].string
 	releaseDate = details[2].string
-	print(title + " " + genre + " " + score + " " + releaseDate)
+	if score :
+		print(title + " " + genre + " " + score + " " + releaseDate)
