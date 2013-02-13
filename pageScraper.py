fe6bcefac490cf759f7f09ac989b66159c90220c
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2013-02-13 12:52:51 +0000

    Basic page scraper finished
    Need to finish save statement

--- pageScraper.py
+++ pageScraper.py
@@ -2,18 +2,34 @@ import urllib2
 
 from bs4 import BeautifulSoup
 
-#shouldn't the code be neater?!
+url = "http://uk.gamespot.com/games.html?platform=1029&mode=all&sort=views&dlx_type=all&sortdir=asc&official=all&page="
+games = []
+pageCounter = 0
+noResults = False
 
-url = "http://uk.gamespot.com/games.html?platform=1029&mode=all&sort=views&dlx_type=all&sortdir=asc&official=all"
-pageSoup = BeautifulSoup(urllib2.urlopen(url))
+while !noResults:
 
-gamesTable = pageSoup.find("tbody").find_all("tr")
+	pageUrl = url + pageCounter
+	pageSoup = BeautifulSoup(urllib2.urlopen(pageUrl))
 
-for game in gamesTable:
-	title = game.find("a").string
-	details = game.find_all("td")
-	genre = details[0].string
-	score = details[1].string
-	releaseDate = details[2].string
-	if score :
-		print(title + " " + genre + " " + score + " " + releaseDate)
+	games = pageSoup.find("tbody").find_all("tr")
+
+	# Extract games from table
+	if(games == []):
+		noResults = true
+	else:
+		for game in games:
+			title = game.find("a").string
+			gameUrl = game.find("a").get('href')
+			details = game.find_all("td")
+			genre = details[0].string
+			score = details[1].string
+			releaseDate = details[2].string
+			if score :
+				#print(title + " " + genre + " " + score + " " + releaseDate + gameUrl)
+				games.push({title:title, genre: genre, score:score, url:gameUrl})
+
+	# Loop through games and get more details
+	#for game in games:
+		#gameUrl = game[url];
+		#gameDetailsSoup = BeautifulSoup(urllib2.urlopen(gameUrl))
