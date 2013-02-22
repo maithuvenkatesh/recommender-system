015fed4d8296149d613aad82caade0176310e165
Maithu Venkatesh <maithuvenkatesh@gmail.com>
2013-02-22 02:06:36 +0000

    Save game script not saving properly to database
    Used logging, information being received but not stored in datastore
    correctly

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ from google.appengine.ext.webapp import template
 class MainHandler(webapp2.RequestHandler):
     def get(self):
     	path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, {}))
+    	self.response.out.write(template.render(path, {}))
 
 app = webapp2.WSGIApplication([('/.*', MainHandler)], debug=True)
--- saveGame.py
+++ saveGame.py
@@ -1,4 +1,3 @@
-import game
 import webapp2
 import os
 import logging
@@ -6,18 +5,32 @@ import logging
 from google.appengine.ext.webapp import template
 from google.appengine.ext import db
 
+class Game(db.Model):
+	title = db.StringProperty
+	year = db.IntegerProperty
+	genre = db.StringProperty
+	platform = db.StringProperty(choices=set(["xbox360","playstation3","pc"]))
+	gamespotScore = db.FloatProperty
+	url = db.LinkProperty
+	#userRating = db.FloatProperty
+	#localMultiplayer = db.BooleanProperty
+	#onlineMultiplayer = db.BooleanProperty
+	#developer = db.StringProperty
+	#publisher = db.StringProperty
+	#Amend platform types
+	#Add in any other features required?
+
 class SaveGameHandler(webapp2.RequestHandler):
     def post(self):
-		self.response.write(self.request.get('title'))
-		newGame = game.Game()
+		logging.info("Title of the game is %s", self.request.get('title'))
+		newGame = Game()
 		newGame.title = self.request.get('title')
-		newGame.year = self.request.get('year')
+		newGame.year = int(self.request.get('year'))
 		newGame.genre = self.request.get('genre')
 		newGame.platform = self.request.get('platform')
-		newGame.gamespotScore = self.request.get('gamespotScore')
+		newGame.gamespotScore = float(self.request.get('gamespotScore'))
 		newGame.url = self.request.get('url')
 
-		#TODO parse string -> float and string -> year
 		newGame.put()
 
 
